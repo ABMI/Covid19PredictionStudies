8ca137c3c8b4483bb5767b83f174b00241719ca5
Seng Chan You <applegna@gmail.com>
2020-04-09 22:44:41 +0900

    add target cohorts for hospitalized covid patients and covid patients requiring icu care

--- CovidSimpleSurvival/R/helpers.R
+++ CovidSimpleSurvival/R/helpers.R
@@ -20,29 +20,29 @@ getData <- function(connectionDetails,
   
   for(i in 1:nrow(cohortVarsToCreate)){
     covSets[[1+i]] <- createCohortCovariateSettings(covariateName = as.character(cohortVarsToCreate$cohortName[i]),
-                                                      covariateId = cohortVarsToCreate$cohortId[i]*1000+456,
-                                                      cohortDatabaseSchema = cohortDatabaseSchema,
-                                                      cohortTable = cohortTable,
-                                                      cohortId = cohortVarsToCreate$atlasId[i],
-                                                      startDay=cohortVarsToCreate$startDay[i], 
-                                                      endDay=endDay,
-                                                      count= as.character(cohortVarsToCreate$count[i]), 
-                                                      ageInteraction = as.character(cohortVarsToCreate$ageInteraction[i]))
+                                                    covariateId = cohortVarsToCreate$cohortId[i]*1000+456,
+                                                    cohortDatabaseSchema = cohortDatabaseSchema,
+                                                    cohortTable = cohortTable,
+                                                    cohortId = cohortVarsToCreate$atlasId[i],
+                                                    startDay=cohortVarsToCreate$startDay[i], 
+                                                    endDay=endDay,
+                                                    count= as.character(cohortVarsToCreate$count[i]), 
+                                                    ageInteraction = as.character(cohortVarsToCreate$ageInteraction[i]))
   }
   
   result <- PatientLevelPrediction::getPlpData(connectionDetails = connectionDetails,
-                                     cdmDatabaseSchema = cdmDatabaseSchema,
-                                     oracleTempSchema = oracleTempSchema, 
-                                     cohortId = as.double(as.character(cohortId)), 
-                                     outcomeIds = as.double(as.character(outcomeIds)), 
-                                     cohortDatabaseSchema = cohortDatabaseSchema, 
-                                     outcomeDatabaseSchema = cohortDatabaseSchema, 
-                                     cohortTable = cohortTable, 
-                                     outcomeTable = cohortTable, 
-                                     cdmVersion = cdmVersion, 
-                                     firstExposureOnly = firstExposureOnly, 
-                                     sampleSize =  sampleSize, 
-                                     covariateSettings = covSets)
+                                               cdmDatabaseSchema = cdmDatabaseSchema,
+                                               oracleTempSchema = oracleTempSchema, 
+                                               cohortId = as.double(as.character(cohortId)), 
+                                               outcomeIds = as.double(as.character(outcomeIds)), 
+                                               cohortDatabaseSchema = cohortDatabaseSchema, 
+                                               outcomeDatabaseSchema = cohortDatabaseSchema, 
+                                               cohortTable = cohortTable, 
+                                               outcomeTable = cohortTable, 
+                                               cdmVersion = cdmVersion, 
+                                               firstExposureOnly = firstExposureOnly, 
+                                               sampleSize =  sampleSize, 
+                                               covariateSettings = covSets)
   
   return(result)
   
@@ -52,7 +52,7 @@ getData <- function(connectionDetails,
 getModel <- function(model = 'SimpleModel.csv'){
   pathToCustom <- system.file("settings", model , package = "CovidSimpleSurvival")
   coefficients <- utils::read.csv(pathToCustom)
-   return(coefficients)
+  return(coefficients)
 }
 
 getPredict <- function(model){
@@ -85,35 +85,55 @@ getPredict <- function(model){
 
 
 getSettings <- function(predictShortTermSurvival,
-            predictLongTermSurvival, 
-            usePackageCohorts){
+                        predictLongTermSurvival, 
+                        usePackageCohorts){
   
   settingR <- c()
   
+  pathToCsv <- system.file("settings", "CohortsToCreate.csv", package = "CovidSimpleSurvival")
+  cohortsToCreate <- utils::read.csv(pathToCsv, stringsAsFactors = F)
+  
+  toList<- expand.grid(cohortsToCreate$cohortId[cohortsToCreate$type=='target'], cohortsToCreate$cohortId[cohortsToCreate$type=='outcome'])
+  colnames(toList) <- c("cohortId", "outcomeId")
+  
   if(predictShortTermSurvival){
-    settingR <- rbind(settingR, c(cohortId = ifelse(usePackageCohorts, 1, 1), 
-                             outcomeId = ifelse(usePackageCohorts, 2, 2),
-                             analysisId = 6, 
-                             model = 'ShortTermSurvival.csv',
-                      riskWindowStart = 0,
-                      startAnchor = 'cohort start',
-                      riskWindowEnd = 60,
-                      endAnchor = 'cohort start'))
+    settingR <- rbind(data.frame(settingR), 
+                      data.frame(cohortId = ifelse(usePackageCohorts, toList$cohortId, 1), 
+                                 outcomeId = ifelse(usePackageCohorts, toList$outcomeId, 2),
+                                 analysisId = ifelse(usePackageCohorts, rep(6,nrow(toList)), 6), 
+                                 model = ifelse(usePackageCohorts, rep('ShortTermSurvival.csv',nrow(toList)), 'ShortTermSurvival.csv'), 
+                                 riskWindowStart = ifelse(usePackageCohorts, rep(0,nrow(toList)), 0),
+                                 startAnchor = rep('cohort start',nrow(toList)),
+                                 riskWindowEnd = ifelse(usePackageCohorts, rep(60,nrow(toList)), 60),
+                                 endAnchor = rep('cohort start',nrow(toList))
+                      )
+    )
   }
   if(predictLongTermSurvival){
     settingR <- rbind(settingR, c(cohortId = ifelse(usePackageCohorts, 1, 1), 
-                             outcomeId = ifelse(usePackageCohorts, 2, 2),
-                             analysisId = 7, 
-                             model = 'LongTermSurvival.csv',
-                      riskWindowStart = 0,
-                      startAnchor = 'cohort start',
-                      riskWindowEnd = 365,
-                      endAnchor = 'cohort start'))
+                                  outcomeId = ifelse(usePackageCohorts, 2, 2),
+                                  analysisId = 7, 
+                                  model = 'LongTermSurvival.csv',
+                                  riskWindowStart = 0,
+                                  startAnchor = 'cohort start',
+                                  riskWindowEnd = 365,
+                                  endAnchor = 'cohort start'))
+    settingR <- rbind(data.frame(settingR), 
+                      data.frame(cohortId = ifelse(usePackageCohorts, toList$cohortId, 1), 
+                                 outcomeId = ifelse(usePackageCohorts, toList$outcomeId, 2),
+                                 analysisId = ifelse(usePackageCohorts, rep(7,nrow(toList)), 7), 
+                                 model = ifelse(usePackageCohorts, rep('LongTermSurvival.csv',nrow(toList)), 'LongTermSurvival.csv'), 
+                                 riskWindowStart = ifelse(usePackageCohorts, rep(0,nrow(toList)), 0),
+                                 startAnchor = ifelse(usePackageCohorts,rep('cohort start',nrow(toList)), 'cohort start'),
+                                 riskWindowEnd = ifelse(usePackageCohorts, rep(365,nrow(toList)), 365),
+                                 endAnchor = rep('cohort start',nrow(toList))
+                      )
+    )
   }
   
   settingR <- as.data.frame(settingR)
   return(settingR)
-
+  
 }
 
 
